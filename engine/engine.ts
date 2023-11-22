2824188dd08be548bb6fdbd9e199e7ee565d862b
Hitesh S <fxnoob71@gmail.com>
2023-11-22 23:03:48 +0530

    Add service factory.

--- engine/engine.ts
+++ engine/engine.ts
@@ -3,7 +3,6 @@ import Bottle from "bottlejs";
 import { baseModules, featureModules } from "./modules.js";
 
 export class Engine {
-  // deno-lint-ignore no-explicit-any
   [x: string]: any;
   constructor() {
     this.app = new Bottle();
@@ -25,33 +24,16 @@ export class Engine {
       if (module.service) {
         this.app.service(module.name, module.service, ...deps);
       }
+      if (module.factoryService) {
+        this.app.serviceFactory(module.name, module.factoryService, ...deps);
+      }
       if (module.factory) {
         this.app.factory(module.name, module.factory);
-      }
-      if (module.init) {
-        module.init(this.app);
       }
     }
-  }
-  async onLoaded() {
-    const modules = [...baseModules, ...featureModules];
     for (const module of modules) {
-      if (!module.name) {
-        throw new Error(
-          "On loading services, we found that Service name is not defined.",
-        );
-      }
-      if (this.app.container[module.name]) {
-        throw new Error(
-          `Same name service already exists. Conflicting name: ${module.name}`,
-        );
-      }
-      const deps = module.deps ? module.deps : [];
-      if (module.service) {
-        this.app.service(module.name, module.service(), ...deps);
-      }
-      if (module.factory) {
-        this.app.factory(module.name, module.factory);
+      if (module.init) {
+        module.init(this.app);
       }
     }
   }
